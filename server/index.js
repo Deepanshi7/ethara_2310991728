32629abe7f7979e61116d0d56cae1f0b67e04bbc
Bavanpreet <bavanpreet.0731@gmail.com>
2026-05-20 22:15:17 +0530

    fix render deployment

--- server/index.js
+++ server/index.js
@@ -525,7 +525,7 @@ app.delete(
   })
 );
 
-const clientDist = path.join(__dirname, "..", "dist", "client");
+const clientDist = path.join(__dirname, "..", "dist");
 app.use(express.static(clientDist));
 app.get(/.*/, (_req, res) => {
   res.sendFile(path.join(clientDist, "index.html"));
